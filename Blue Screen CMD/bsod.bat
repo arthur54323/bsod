1fd409f4db380bb334a9399b3a01fb8ba0d42fec
arthur54323 <130246135+arthur54323@users.noreply.github.com>
2023-07-02 20:43:15 -0300

    the folder
    WARNING, ONLY USE IN VM

---
+++ Blue Screen CMD/bsod.bat
@@ -0,0 +1,19 @@
+@echo off
+
+color a
+echo Hello, you you want the bsod? is critical process died, you want or no (answer only yes/no)
+set /p input=
+if /i %input%==Yes goto want
+if /i %input%==No goto dont want
+if /i not %input%== Yes,no goto 1
+
+:no
+echo ok buddie;)
+pause
+exit
+
+:yes
+echo ok..
+echo get warned
+timeout 10
+powershell wininit
---
+++ Blue Screen CMD/readme.txt
@@ -0,0 +1,7 @@
+warning, you are using theres programs thats bsod your pc.
+
+the stop code is only
+
+CRITICAL_PROCESS_DIED (TRY ONLY ON YOUR VIRTUAL MACHINE)
+
+so pls dont try on your real computer
